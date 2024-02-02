cc9402ae3e03bf3f8149a3eae7c726b2e27f376e
asafzaf <asaf.zafrir@gmail.com>
2024-02-02 15:44:11 +0200

    changed to custom buttons.

--- screens/newFault/NewFaultScreens.js
+++ screens/newFault/NewFaultScreens.js
@@ -1,23 +1,28 @@
 import { Button, StyleSheet, Text, View } from "react-native";
 import * as React from "react";
+import BigSelectionButton from "../../components/buttons/BigSelectionButton";
+import { styles } from "../../styles";
+ 
 
 function NewFaultScreen({ navigation }) {
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>אנא בחר:</Text>
-        <Button
-          color="#FA0606"
-          title="בפנים"
-          onPress={() => navigation.push("Inside")}
-        />
-        <Button
-          color="#060AFA"
-          title="בחוץ"
-          onPress={() => navigation.push("Outside")}
-        />
-        <Button title="חזרה לתפריט ראשי" onPress={() => navigation.popToTop()} />
-      </View>
-    );
-  }
-  
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={styles.boldText}>אנא בחר:</Text>
+      <BigSelectionButton
+        colors={["#C37E49", "#E19154"]}
+        logo={require('../../assets/House-03-WF.png')}
+        title="בפנים"
+        onPress={() => navigation.push("Inside")}
+      ></BigSelectionButton>
+      <BigSelectionButton
+        colors={["#63BD44","#7AE058"]}
+        logo={require('../../assets/Tree-WF1.png')}
+        title="בחוץ"
+        onPress={() => navigation.push("Outside")}
+      ></BigSelectionButton>
+      <Button title="חזרה לתפריט ראשי" onPress={() => navigation.popToTop()} />
+    </View>
+  );
+}
+
 export default NewFaultScreen;
