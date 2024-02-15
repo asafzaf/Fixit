3bbab5c00a04940f3c7fd720e9eacb1c68e23044
asafzaf <asaf.zafrir@gmail.com>
2024-02-15 13:11:47 +0200

    ADDED 3 NEW SCREENS: SPACE CHOOSER FAULT CHOSSER CONFIRMTION

--- stacks/NewFaultStack.js
+++ stacks/NewFaultStack.js
@@ -2,6 +2,9 @@ import * as React from "react";
 import InsidePlaceScreen from "../screens/newFault/InsidePlaceScreen";
 import OutsidePlaceScreen from "../screens/newFault/OutsidePlaceScreen";
 import NewFaultScreen from "../screens/newFault/NewFaultScreens";
+import SpaceChooseScreen from "../screens/newFault/SpaceChooseScreen";
+import FaultChooseScreen from "../screens/newFault/FaultChooseScreen";
+import ConfirmFaultScreen from "../screens/newFault/ConfirmFaultScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -27,6 +30,24 @@ function NewFaultRoot({ navigation }) {
           title="בחוץ"
           component={OutsidePlaceScreen}
           options={{ title: "תקלה מחוץ למבנה" }}
+        />
+        <Stack.Screen 
+          name="SpaceChooser"
+          title="בחר מרחב"
+          component={SpaceChooseScreen}
+          options={{ title: "בחר מרחב" }}
+        />
+        <Stack.Screen
+        name="FaultChoose"
+        title="בחר תקלה"
+        component={FaultChooseScreen}
+        options={{ title: "בחר תקלה" }}
+        />
+        <Stack.Screen
+        name="Confirm"
+        title="אישור"
+        component={ConfirmFaultScreen}
+        options={{ title: "אישור" }}
         />
       </Stack.Navigator>
     );
