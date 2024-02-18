e0c4294fadb0401d3282db4b89608b380ed2d498
Eliya <eliysamaea@gmail.com>
2024-02-18 19:21:22 +0200

    update

--- App.js
+++ App.js
@@ -4,10 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import * as React from "react";
 import NewFaultRoot from "./stacks/NewFaultStack";
-import BigSelectionButton from "./components/buttons/BigSelectionButton";
 import HomePageBtn from "./components/buttons/HomePageBtn";
-import { styles } from "./styles";
-import OpenNewFaultScreen from "./screens/newFault/OpenNewFaultScreen";
 import FixitHeader from "./components/header";
 
 function HomeScreen({ navigation }) {
@@ -21,14 +18,17 @@ function HomeScreen({ navigation }) {
           flex: 1,
           alignItems: "center",
           justifyContent: "center",
+          flexDirection: "column",
         }}
       >
         <View
-          style={{
-            flex: 1,
-            // justifyContent: "flex-start",
-            // backgroundColor: "white",
-          }}
+          style={
+            {
+              // flex: 1,
+              // flexDirection: "column",
+              // justifyContent: "space-between",
+            }
+          }
         >
           <HomePageBtn
             colors={["#0100D8", "#3C3BFA"]}
