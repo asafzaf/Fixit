673b98f648c98a75262b162db584ff74e8233ad8
Eliya <eliysamaea@gmail.com>
2024-02-20 09:12:09 +0200

    navigation update and style update

--- App.js
+++ App.js
@@ -4,35 +4,36 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import * as React from "react";
 import NewFaultRoot from "./stacks/NewFaultStack";
+import MainStack from "./stacks/mainStack";
 import HomePageBtn from "./components/buttons/HomePageBtn";
 import FixitHeader from "./components/header";
 
 function HomeScreen({ navigation }) {
+  // function pressHistoryHandler() {
+  //   navigation.navigate("History");
+  // }
+  // function pressStatusHandler() {
+  //   navigation.navigate("Status");
+  // }
   return (
     <View
-      style={{ flex: 1, alignItems: "center", justifyContent: "flex-start" }}
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "flex-start",
+      }}
     >
       <FixitHeader></FixitHeader>
       <View
         style={{
           flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-          flexDirection: "column",
+          margin: 100,
         }}
       >
-        <View
-          style={
-            {
-              // flex: 1,
-              // flexDirection: "column",
-              // justifyContent: "space-between",
-            }
-          }
-        >
+        <View>
           <HomePageBtn
             colors={["#0100D8", "#3C3BFA"]}
-            logo={require("./assets/Tools-01.png")}
+            logo={require("./assets/addFaultIcon.png")}
             title="Open New Fault"
             onPress={() =>
               navigation.navigate("Root", { screen: "Open Fault" })
@@ -40,19 +41,21 @@ function HomeScreen({ navigation }) {
           ></HomePageBtn>
           <HomePageBtn
             colors={["#0100D8", "#3C3BFA"]}
-            logo={require("./assets/Tools-01.png")}
+            logo={require("./assets/statusIcon.png")}
             title="Faults Status"
-            onPress={() =>
-              navigation.navigate("Root", { screen: "Open Fault" })
-            }
+            // onPress={pressStatusHandler}
+            // onPress={() =>
+            //   navigation.navigate("mainStack", { screen: "Status" })
+            // }
           ></HomePageBtn>
           <HomePageBtn
             colors={["#0100D8", "#3C3BFA"]}
-            logo={require("./assets/Tools-01.png")}
+            logo={require("./assets/historyIcon.png")}
             title="History"
-            onPress={() =>
-              navigation.navigate("Root", { screen: "Open Fault" })
-            }
+            // onPress={pressHistoryHandler}
+            // onPress={() =>
+            //   navigation.navigate("mainStack", { screen: "History" })
+            // }
           ></HomePageBtn>
         </View>
       </View>
@@ -65,14 +68,20 @@ const Stack = createNativeStackNavigator();
 function App() {
   return (
     <>
-      <StatusBar style="auto" />
+      <StatusBar style="dark" />
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen
             name="Home"
             component={HomeScreen}
-            options={{ title: "Home Page" }}
+            options={{ title: "" }}
           />
+          {/* <Stack.Screen
+            name="mainStack"
+            title=""
+            component={MainStack}
+            options={{ headerShown: false }}
+          /> */}
           <Stack.Screen
             name="Root"
             title="Open New Fault"
