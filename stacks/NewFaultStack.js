c4e00f7e27ef26a64bc434a2b798e4f54c2b77fd
Eliya <eliysamaea@gmail.com>
2024-02-21 16:45:45 +0200

    adding screen to stack

--- stacks/NewFaultStack.js
+++ stacks/NewFaultStack.js
@@ -8,6 +8,7 @@ import ConfirmFaultScreen from "../screens/newFault/ConfirmFaultScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import OpenNewFaultScreen from "../screens/newFault/OpenNewFaultScreen";
+import specificSpaceInBuilding from "../screens/newFault/specificSpaceInBuilding";
 
 const Stack = createNativeStackNavigator();
 
@@ -33,22 +34,28 @@ function NewFaultRoot({ navigation }) {
         options={{ title: "Step 2" }}
       />
       <Stack.Screen
-        name="SpaceChooser"
+        name="SpaceTypeChooser"
         title="Choose Space"
         component={spaceInBuilding}
         options={{ title: "Step 3" }}
       />
       <Stack.Screen
+        name="SpaceChooser"
+        title="Choose Space"
+        component={specificSpaceInBuilding}
+        options={{ title: "Step 4" }}
+      />
+      <Stack.Screen
         name="FaultChoose"
         title="Choose Fault Type"
         component={FaultChooseScreen}
-        options={{ title: "Step 4" }}
+        options={{ title: "Step 5" }}
       />
       <Stack.Screen
         name="Confirm"
         title="Fault Confirmation"
         component={ConfirmFaultScreen}
-        options={{ title: "Step 5" }}
+        options={{ title: "Step 6" }}
       />
     </Stack.Navigator>
   );
