338cce6ed3b3013611420c1640d60131efde39ea
Eliya <eliysamaea@gmail.com>
2024-02-22 21:08:42 +0200

    adding style

--- components/FaultStatusGrid.js
+++ components/FaultStatusGrid.js
@@ -8,30 +8,39 @@ const inProgressColor = "#724502";
 const styles = StyleSheet.create({
   item: {
     marginTop: 20,
-    width: "85%",
+    width: "70%",
     borderRadius: 10,
     backgroundColor: "white",
     flex: 2,
     flexDirection: "row",
     justifyContent: "space-between",
-    shadowColor: "black",
-    shadowOffset: { width: -2, height: 4 },
-    shadowOpacity: 0.2,
-    shadowRadius: 2,
+    // shadowColor: "black",
+    // shadowOffset: { width: -2, height: 4 },
+    // shadowOpacity: 0.2,
+    // shadowRadius: 2,
   },
   status: {
-    backgroundColor: pendingColor,
+    // backgroundColor: color,
     borderTopRightRadius: 10,
     borderBottomRightRadius: 10,
     height: "100%",
     width: "50%",
   },
+  id: {
+    backgroundColor: "white",
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
+  },
   text: {
     margin: 10,
-    fontSize: 16,
+    fontSize: 12,
+    // textAlign: "left",
   },
   status_text: {
-    margin: 10,
+    // margin: 10,
+    padding: 10,
     fontSize: 16,
     color: "white",
     fontWeight: "bold",
@@ -39,16 +48,25 @@ const styles = StyleSheet.create({
   },
 });
 
-function FaultsGrid({ id }) {
+function FaultsGrid({ id, status }) {
+  let color =
+    status === ("Pending" || "pending")
+      ? pendingColor
+      : status === ("Closed" || "closed")
+      ? closedColor
+      : inProgressColor;
+
+  console.log(color);
+  console.log(status);
   return (
     <View>
       <Pressable>
         <View style={styles.item}>
-          <View>
-            <Text style={styles.text}>hey</Text>
+          <View style={styles.id}>
+            <Text style={styles.text}>{id}</Text>
           </View>
-          <View style={styles.status}>
-            <Text style={styles.status_text}>bfgny</Text>
+          <View style={[styles.status, { backgroundColor: color }]}>
+            <Text style={styles.status_text}>{status}</Text>
           </View>
         </View>
       </Pressable>
