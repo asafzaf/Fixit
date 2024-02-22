217d64a3583d165ba674ed2009f4284536869d48
Eliya <eliysamaea@gmail.com>
2024-02-22 17:35:03 +0200

    adding scroll view

--- screens/newFault/insideOrOutside.js
+++ screens/newFault/insideOrOutside.js
@@ -1,6 +1,6 @@
 // 1st PAGE OF THE FAULT REPORTING PROCESS
 
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, ScrollView, Text, View } from "react-native";
 import * as React from "react";
 import BigSelectionButton from "../../components/buttons/BigSelectionButton";
 import { styles } from "../../styles";
@@ -19,22 +19,26 @@ function NewFaultScreen({ navigation }) {
         }}
       >
         <Text style={styles.small_title}>Where are you ?</Text>
-        <BigSelectionButton
-          colors={["#0100D8", "#4A49FF"]}
-          logo={require("../../assets/House-03-WF.png")}
-          title="Inside"
-          onPress={() => navigation.push("Inside")}
-        ></BigSelectionButton>
-        <BigSelectionButton
-          colors={["#6416F8", "#9158FF"]}
-          logo={require("../../assets/Tree-WF1.png")}
-          title="Outside"
-          onPress={() => navigation.push("Outside")}
-        ></BigSelectionButton>
-        <Button
-          title="Back to Home Page"
-          onPress={() => navigation.popToTop()}
-        />
+        <ScrollView>
+          <BigSelectionButton
+            colors={["#0100D8", "#4A49FF"]}
+            logo={require("../../assets/House-03-WF.png")}
+            title="Inside"
+            onPress={() => navigation.push("Inside")}
+          ></BigSelectionButton>
+          <BigSelectionButton
+            colors={["#6416F8", "#9158FF"]}
+            logo={require("../../assets/Tree-WF1.png")}
+            title="Outside"
+            onPress={() => navigation.push("Outside")}
+          ></BigSelectionButton>
+        </ScrollView>
+        <View style={{ marginBottom: 50 }}>
+          <Button
+            title="Back to Home Page"
+            onPress={() => navigation.popToTop()}
+          />
+        </View>
       </View>
     </View>
   );
