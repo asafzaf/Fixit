6c321b41438d4bf8d0408e107c8a2f6cb9928222
asafzaf <asaf.zafrir@gmail.com>
2024-02-24 21:44:26 +0200

    added context to app.

--- App.js
+++ App.js
@@ -1,16 +1,29 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import * as React from "react";
+import { useContext } from "react";
 
 import HomeScreenStack from "./stacks/HomeScreenStack";
+import AuthStack from "./stacks/AuthStack";
+import AuthContextProvider, { AuthContext } from "./store/auth-context";
+
+function Navigation() {
+  const authCtx = useContext(AuthContext);
+  return (
+    <NavigationContainer>
+      {!authCtx.isAuthenticated && <AuthStack />}
+      {authCtx.isAuthenticated && <HomeScreenStack />}
+    </NavigationContainer>
+  );
+}
 
 function App() {
   return (
     <>
       <StatusBar style="dark" />
-      <NavigationContainer>
-        <HomeScreenStack />
-      </NavigationContainer>
+      <AuthContextProvider>
+        <Navigation />
+      </AuthContextProvider>
     </>
   );
 }
