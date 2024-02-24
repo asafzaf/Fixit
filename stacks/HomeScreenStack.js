82b71888e5a4e8a4b855ae8fd4bfd2eb07f1292f
asafzaf <asaf.zafrir@gmail.com>
2024-02-24 21:39:54 +0200

    added context and logout button.

--- stacks/HomeScreenStack.js
+++ stacks/HomeScreenStack.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreenUser from "../screens/home/HomeScreenUser";
 import NewFaultRoot from "./NewFaultStack";
 import MainStack from "./MainStack";
+import IconButton from "../components/buttons/IconButton";
+import { AuthContext } from "../store/auth-context";
 
 const Stack = createNativeStackNavigator();
 
 export default function HomeScreenStack() {
+  const authCtx = useContext(AuthContext);
   return (
     <Stack.Navigator>
       <Stack.Screen
         name="Home"
         component={HomeScreenUser}
-        options={{ title: "" }}
+        options={{
+          title: "",
+          headerRight: () => (
+            <IconButton
+              icon="exit"
+              color="#0D24F1"
+              size={24}
+              onPress={authCtx.logout}
+            />
+          ),
+        }}
       />
       <Stack.Screen
         name="MainStack"
