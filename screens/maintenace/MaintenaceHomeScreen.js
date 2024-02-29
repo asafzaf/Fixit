2b05434feabcf9725fb8332ebea3f4c02defd51b
asafzaf <asaf.zafrir@gmail.com>
2024-02-29 13:42:32 +0200

    added basic pie chart

--- screens/maintenace/MaintenaceHomeScreen.js
+++ screens/maintenace/MaintenaceHomeScreen.js
@@ -1,18 +1,19 @@
-import React, { useContext } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-import { AuthContext } from '../../store/auth-context';
-import { styles } from '../../styles';
+import React, { useContext } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { AuthContext } from "../../store/auth-context";
+import { styles } from "../../styles";
+import StatusChart from "../../components/Maintenace/StatusChart";
 
-
-const MaintenaceHomeScreen = ({navigation}) => {
-    const authCtx = useContext(AuthContext);
-    return (
-        <View style={styles.container}>
-        <Text style={styles.small_title}>Maintenance Home Screen</Text>
-        <Text style={styles.small_title}>Welcome {authCtx.name}</Text>
-        <Button title="Logout" onPress={authCtx.logout} />
-        </View>
-    );
-    }
+const MaintenaceHomeScreen = ({ navigation }) => {
+  const authCtx = useContext(AuthContext);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.small_title}>Maintenance Home Screen</Text>
+      <Text style={styles.small_title}>Welcome {authCtx.name}</Text>
+      <StatusChart />
+      <Button title="Logout" onPress={authCtx.logout} />
+    </View>
+  );
+};
 
 export default MaintenaceHomeScreen;
