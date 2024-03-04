9b5ca4805c07ba0bc89a4b970a3e315b9c045e6b
Eliya <eliysamaea@gmail.com>
2024-03-04 15:02:55 +0200

    adding screen

---
+++ screens/newFault/OutsideSpaceScreen.js
@@ -0,0 +1,64 @@
+// ADD TO LOWER CASE !!!!!
+
+import { FlatList, Text, View } from "react-native";
+import { styles } from "../../styles";
+import SmallSelectionButton from "../../components/buttons/smallSelectionButton";
+import TitleHeader from "../../components/headerTitle";
+import OutsideSpacesGrid from "../../components/outsideSpacesGrid";
+
+function OutsideSpaceScreen({ navigation, route }) {
+  const outsideId = route.params.outsideId;
+  const outsideName = route.params.outsideName;
+  const data = route.params.data;
+
+  const dataDisplay = [];
+
+  for (const space of data.spaces) {
+    dataDisplay.push(space);
+  }
+
+  //   console.log(dataDisplay);
+
+  function renderSpaces({ item }) {
+    return (
+      <OutsideSpacesGrid
+        // navigation={navigation}
+        name={item.spaceName}
+        nameHebrew={item.spaceNameHeb}
+        spaceType={item.spaceType}
+        outsideId={outsideId}
+        outsideName={outsideName}
+      />
+    );
+  }
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "flex-start",
+      }}
+    >
+      <TitleHeader title={"Open New Fault"}></TitleHeader>
+      <View
+        style={{
+          marginTop: 20,
+          alignItems: "center",
+        }}
+      >
+        <Text style={styles.small_title}>Select Space</Text>
+        <View>
+          <FlatList
+            data={dataDisplay}
+            keyEtrator={(item) => item.spaceName}
+            renderItem={renderSpaces}
+            numColumns={2}
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+export default OutsideSpaceScreen;
