d455d6afb94cb4d0903a43278003e1f5474a980f
Eliya <eliysamaea@gmail.com>
2024-03-04 12:42:40 +0200

    test

--- components/Auth/AuthForm.js
+++ components/Auth/AuthForm.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { StyleSheet, View } from "react-native";
 
+// test
 // import Button from '../ui/Button';
 import Input from "./Input";
 import ConfirmButton from "../buttons/ConfirmButton";
