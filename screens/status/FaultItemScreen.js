13b1f08fd3dc8e9132f9fa148ff27839965cfa0a
Eliya <eliysamaea@gmail.com>
2024-03-06 15:28:33 +0200

    Creating a separation between outside and inside(buildings)

--- screens/status/FaultItemScreen.js
+++ screens/status/FaultItemScreen.js
@@ -51,6 +51,88 @@ const FaultItemScreen = ({ navigation, route }) => {
     );
   }
 
+  if (fetchedFault.data?.fault.buildingName) {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <TitleHeader title={"Fault Details"}></TitleHeader>
+        <ScrollView contentContainerStyle={{ paddingBottom: 100 }}>
+          <View
+            contentContainerStyle={{ width: "100%", justifyContent: "center" }}
+          >
+            {err && <Text>{err}</Text>}
+            {!isFetching && !err && (
+              <View style={styles.text_container}>
+                <Text style={styles.text_primary}></Text>
+                {/* <Text>{JSON.stringify(fetchedFault)}</Text> */}
+                <View style={styles.text_container_secondary}>
+                  <Text style={styles.text_secondary}>Building:</Text>
+                  <Text style={styles.text_primary}>
+                    {fetchedFault.data?.fault.buildingName}
+                  </Text>
+                </View>
+                <View style={styles.text_container_secondary}>
+                  <Text style={styles.text_secondary}>Floor Number:</Text>
+                  <Text style={styles.text_primary}>
+                    {fetchedFault.data?.fault.floor}
+                  </Text>
+                </View>
+                <View style={styles.text_container_secondary}>
+                  <Text style={styles.text_secondary}>Fault:</Text>
+                  <Text style={styles.text_primary}>
+                    {fetchedFault.data?.fault.faultTypeNameEng}
+                  </Text>
+                </View>
+                <View style={styles.text_container_secondary}>
+                  <Text style={styles.text_secondary}>Space:</Text>
+                  <Text style={styles.text_primary}>
+                    {fetchedFault.data?.fault.spaceTypeNameEng}
+                  </Text>
+                </View>
+                <View style={styles.text_container_secondary}>
+                  <Text style={styles.text_secondary}>Fault Status:</Text>
+                  <Text style={styles.text_primary}>
+                    {fetchedFault.data?.fault.status}
+                  </Text>
+                </View>
+                <View style={styles.text_container_description}>
+                  <Text style={styles.text_secondary}>Fault Description:</Text>
+                  <View style={styles.text_container_third}>
+                    <Text style={styles.text_primary}>
+                      {fetchedFault.data?.fault.description}
+                    </Text>
+                  </View>
+                </View>
+              </View>
+            )}
+            {!isFetching && !err && havePhoto && (
+              <View>
+                <View style={styles.image_container}>
+                  <Text style={styles.text_secondary}>Fault Image:</Text>
+                  <Image
+                    source={{
+                      uri:
+                        "https://fixit-gjwz.onrender.com/api/v1/image/" +
+                        fetchedFault.data?.fault.photo.split(".")[0],
+                    }}
+                    style={{
+                      width: 200,
+                      height: 200,
+                      marginTop: 20,
+                      marginBottom: 20,
+                      borderRadius: 10,
+                    }}
+                  />
+                </View>
+              </View>
+            )}
+          </View>
+        </ScrollView>
+        <View style={{ marginBottom: 70 }}>
+          <Button title="Go Back" onPress={() => navigation.popToTop()} />
+        </View>
+      </View>
+    );
+  }
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <TitleHeader title={"Fault Details"}></TitleHeader>
@@ -62,19 +144,6 @@ const FaultItemScreen = ({ navigation, route }) => {
           {!isFetching && !err && (
             <View style={styles.text_container}>
               <Text style={styles.text_primary}></Text>
-              {/* <Text>{JSON.stringify(fetchedFault)}</Text> */}
-              <View style={styles.text_container_secondary}>
-                <Text style={styles.text_secondary}>Building:</Text>
-                <Text style={styles.text_primary}>
-                  {fetchedFault.data?.fault.buildingName}
-                </Text>
-              </View>
-              <View style={styles.text_container_secondary}>
-                <Text style={styles.text_secondary}>Floor Number:</Text>
-                <Text style={styles.text_primary}>
-                  {fetchedFault.data?.fault.floor}
-                </Text>
-              </View>
               <View style={styles.text_container_secondary}>
                 <Text style={styles.text_secondary}>Outdoor:</Text>
                 <Text style={styles.text_primary}>
@@ -93,17 +162,25 @@ const FaultItemScreen = ({ navigation, route }) => {
                   {fetchedFault.data?.fault.spaceTypeNameEng}
                 </Text>
               </View>
-              <View style={styles.text_container_secondary}>
+              {/* <View style={styles.text_container_secondary}>
                 <Text style={styles.text_secondary}>Fault Description:</Text>
                 <Text style={styles.text_primary}>
                   {fetchedFault.data?.fault.description}
                 </Text>
-              </View>
+              </View> */}
               <View style={styles.text_container_secondary}>
                 <Text style={styles.text_secondary}>Fault Status:</Text>
                 <Text style={styles.text_primary}>
                   {fetchedFault.data?.fault.status}
                 </Text>
+              </View>
+              <View style={styles.text_container_description}>
+                <Text style={styles.text_secondary}>Fault Description:</Text>
+                <View style={styles.text_container_third}>
+                  <Text style={styles.text_primary}>
+                    {fetchedFault.data?.fault.description}
+                  </Text>
+                </View>
               </View>
             </View>
           )}
@@ -166,26 +243,41 @@ const styles = StyleSheet.create({
     alignContent: "center",
     flexWrap: "wrap",
   },
-  text_secondary: {
-    fontWeight: "bold",
-    fontSize: 20,
+  text_container_third: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    alignContent: "center",
+    flexWrap: "wrap",
+    marginTop: 7,
   },
-  image_container: {
-    position: "relative",
-
-    // display: "flex",
-    // flexDirection: "row",
-    // gap: 5,
+  text_container_description: {
+    display: "flex",
+    flexDirection: "row",
     backgroundColor: "white",
     padding: 5,
     paddingLeft: 20,
     width: 300,
-    // height: 70,
+    height: 100,
     borderRadius: 10,
     margin: 5,
-    // alignItems: "center",
-    // alignContent: "center",
-    // flexWrap: "wrap",
+    flexWrap: "wrap",
+  },
+  text_secondary: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  text_third: {
+    fontSize: 18,
+  },
+  image_container: {
+    position: "relative",
+    backgroundColor: "white",
+    padding: 5,
+    paddingLeft: 20,
+    width: 300,
+    borderRadius: 10,
+    margin: 5,
   },
   text_primary: {
     fontSize: 18,
